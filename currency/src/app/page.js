d73e41b92f0620cf1570cd36c9a606eb9c6f435e
ThenuKalyan <thenammainachiyappan@ThenuKalyans-MacBook-Air.local>
2025-04-09 20:12:10 -0700

    added input feild

--- currency/src/app/page.js
+++ currency/src/app/page.js
@@ -1,17 +1,36 @@
+'use client'
 import Image from "next/image";
+import {useState ,useEffect} from "react";
 
 export default function Home() {
+  const [amount,setAmount]= useState(0);
+  const [convertedAmount,setConvertedAmount] = useState(0 );
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+        <h1 className="text-3xl sm:text-4xl font-bold text-center sm:text-left">
+        Currency Converter
+        </h1>
+        <input 
+        type="number"
+        placeholder="Enter amount"
+        className="p-2 border border-solid border-black/[.08] dark:border-white/[.145] rounded-full placeholder:text-center sm:placeholder:text-left"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
         />
+        <input 
+        type="number"
+        placeholder="Enter amount"
+        className="p-2 border border-solid border-black/[.08] dark:border-white/[.145] rounded-full placeholder:text-center sm:placeholder:text-left"
+        value={convertedAmount}
+        onChange={(e) => setConvertedAmount(e.target.value)}
+        />
+        
+        
+        
+        
+        
+        
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
@@ -51,53 +70,7 @@ export default function Home() {
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+     
     </div>
   );
 }
